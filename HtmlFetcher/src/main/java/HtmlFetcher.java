b1cc39b46275062dc04404b0a21df496b630f072
JeromeRReduta <38510370+JeromeRReduta@users.noreply.github.com>
2020-11-13 22:52:07 -0800

    Escaped \s

--- HtmlFetcher/src/main/java/HtmlFetcher.java
+++ HtmlFetcher/src/main/java/HtmlFetcher.java
@@ -48,7 +48,7 @@ public class HtmlFetcher {
 	 * @return the HTTP status code or -1 if unable to parse for any reasons
 	 */
 	public static int getStatusCode(Map<String, List<String>> headers) {
-		String statusCode = headers.get(null).get(0).split("\s")[1];
+		String statusCode = headers.get(null).get(0).split("\\s+")[1];
 		return Integer.parseInt(statusCode);
 		
 	}
